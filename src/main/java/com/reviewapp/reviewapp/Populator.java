a1e120321eeff27ed45f26d6a926232ecfa3b139
simonjv01 <sjvargas01@gmail.com>
2020-06-27 09:04:15 -0400

    images update

--- src/main/java/com/reviewapp/reviewapp/Populator.java
+++ src/main/java/com/reviewapp/reviewapp/Populator.java
@@ -25,19 +25,20 @@ public class Populator implements CommandLineRunner {
         Category vacation = new Category("Vacation Spots");
         categoryRepo.save(vacation);
 
-        Review dellLaptop = new Review("Dell Laptop","Computer",technology,"lightweight powerhouse","./css/images/dell.jpg");
-        Review macLaptop = new Review("Macbook Pro Laptop","Computer",technology,"design powerhouse","./images/mac.jpg");
-        Review lenovoLaptop = new Review("Lenovo Laptop","Computer",technology,"Windows best laptop","./images/lenovo.jpg");
+        Review dellLaptop = new Review("Dell Laptop","Computer",technology,"lightweight powerhouse","/images/dell.jpg");
+        Review macLaptop = new Review("Macbook Pro Laptop","Computer",technology,"design powerhouse","/images/mac.jpg");
+        Review lenovoLaptop = new Review("Lenovo Laptop","Computer",technology,"Windows best laptop","/images/lenovo.jpg");
 
-        Review mexicanFood = new Review("Best Mexican Food","El Vaquero",restaurant,"authentic cuisine","./images/mexican.jpg");
-        Review italianFood = new Review("Best Italian Food","Brio",restaurant,"authentic cuisine","./images/italian.jpg");
-        Review americanFood = new Review("Best American Food","Thurman's Cafe",restaurant,"authentic cuisine","./images/american.jpg");
+        Review mexicanFood = new Review("Best Mexican Food","El Vaquero",restaurant,"authentic cuisine",
+                "/images/mexican.jpg");
+        Review italianFood = new Review("Best Italian Food","Brio",restaurant,"authentic cuisine","/images/italian.jpg");
+        Review americanFood = new Review("Best American Food","Thurman's Cafe",restaurant,"authentic cuisine","/images/american.jpg");
 
-        Review avengers = new Review("Avengers Endgame","Best team superhero movie",movies,"Avengers vs Thanos","./images/avengers.jpg");
-        Review spiderman = new Review("Spiderman 3","Best Spiderman movie",movies,"I forget what this one is about","./images/spiderman.jpg");
+        Review avengers = new Review("Avengers Endgame","Best team superhero movie",movies,"Avengers vs Thanos","/images/avengers.jpg");
+        Review spiderman = new Review("Spiderman 3","Best Spiderman movie",movies,"I forget what this one is about","/images/spiderman.jpg");
 
-        Review maui = new Review("Maui: Best beaches","Maui's paradise",vacation,"best beaches in world","./images/maui.jpg");
-        Review egypt = new Review("Egypt: Best sight seeing","Pyramids",vacation,"ancient architecture","./images/egypt.jpg");
+        Review maui = new Review("Maui: Best beaches","Maui's paradise",vacation,"best beaches in world","/images/maui.jpg");
+        Review egypt = new Review("Egypt: Best sight seeing","Pyramids",vacation,"ancient architecture","/images/egypt.jpg");
 
         reviewRepo.save(dellLaptop);
         reviewRepo.save(macLaptop);
